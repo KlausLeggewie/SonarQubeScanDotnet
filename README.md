a42aa5c16c38ac29ab1d4bd802e8a8f7aee932b4
Klaus Leggewie <klaus.leggewie@syzygy.de>
2022-04-16 15:23:52 +0200

    added docker compose setup for dotnet sdk and sonar scanner

--- README.md
+++ README.md
@@ -21,4 +21,18 @@ Next:
 3. Generate a login token => will be required later on
 
 ## Sonar Scanner for .NET
-(tbc)
+
+The Sonar Scanner for .NET  runs locally and is installed in a .NET 6.0 SDK docker image.
+Installation follows https://docs.sonarqube.org/latest/analysis/scan/sonarscanner-for-msbuild/ .
+
+All configurations are done in the .env file:
+* SONAR_PROJECT_KEY: key of sonar project defined by sonar server
+* SONAR_URL: base url sonar server
+* SONAR_TOKEN: token of scan-user, defined by sonar server
+* DOTNET_SOURCE_DIR: local source directory, will be bound to /src
+* DOTNET_BUILD_SOLUTION_PATH: path of solution or project, relative to /src
+
+In the sonarscanner directory execute:
+```bash
+docker compose run dotnet
+```
---
+++ sonarscanner/dotnet-scan-build.sh
@@ -0,0 +1,22 @@
+echo "** started dotnet-scan-build"
+
+DOTNET_BUILD_SOLUTION_PATH="/src/"${DOTNET_BUILD_SOLUTION_PATH}
+
+echo ${SONAR_PROJECT_KEY}
+echo ${SONAR_URL}
+echo ${SONAR_TOKEN}
+echo ${DOTNET_BUILD_SOLUTION_PATH}
+
+echo "** install dotnet scanner"
+export LC_ALL=C.UTF-8
+dotnet tool install --global dotnet-sonarscanner --version 5.5.3
+dotnet tool install --global dotnet-reportgenerator-globaltool
+export PATH="$PATH:/root/.dotnet/tools"
+apt update && apt install -yyq openjdk-11-jre
+echo "** dotnet sonarscanner begin"
+dotnet sonarscanner begin /k:${SONAR_PROJECT_KEY} /d:sonar.host.url=${SONAR_URL} /d:sonar.login=${SONAR_TOKEN}
+echo "** dotnet build"
+dotnet build ${DOTNET_BUILD_SOLUTION_PATH}
+echo "** dotnet sonarscanner end"
+dotnet sonarscanner end /d:sonar.login=${SONAR_TOKEN}
+echo "** finished dotnet-scan-build"
